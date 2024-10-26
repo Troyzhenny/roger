80409a748ca0dbd587438c25ea60cfdc5a37b6a5
Tevin Campbell <tevincampbell73@yahoo.com>
2024-10-26 18:07:33 -0400

    main home bar

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,13 @@
+import PrimaryBtn from "./PrimaryBtn";
+
+const TopBar = () => {
+    return(
+        <>
+        <div className="w-full p-3 items-center flex">
+            <PrimaryBtn />
+        </div>
+        </>
+    )
+}
+
+export default TopBar;
