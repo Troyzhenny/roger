0a3522aca9795cafa819589474ed85ebe3b947f9
Tevin Campbell <tevincampbell73@yahoo.com>
2024-10-27 07:06:30 -0400

    added logic to the add expense button to open the modal

--- src/components/PrimaryBtn.jsx
+++ src/components/PrimaryBtn.jsx
@@ -1,14 +1,19 @@
-import add from '/add.svg'
+import add from '/add.svg';
+import PropTypes from 'prop-types';
 
-const PrimaryBtn = () => {
+const PrimaryBtn = ({ handleModalView }) => {
   return (
     <>
-        <button className="font-bold h-8 w-auto justify-center rounded-full flex items-center gap-1">
+        <button className="font-bold h-8 w-auto justify-center rounded-full flex items-center gap-1" onClick={handleModalView}>
           Expense
           <img src={add} alt="icon" className='w-3'/>
         </button>
     </>
   )
 };
+
+PrimaryBtn.propTypes = {
+  handleModalView: PropTypes.func.isRequired
+}
 
 export default PrimaryBtn;
