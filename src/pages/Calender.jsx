60abe69c522261e1f54736d4614b55f389e69aab
Tevin Campbell <tevincampbell73@yahoo.com>
2024-10-27 08:39:13 -0400

    centered text on pages

--- src/pages/Calender.jsx
+++ src/pages/Calender.jsx
@@ -1,8 +1,10 @@
 const Calender = () => {
     return(
         <>
+        <div className="w-full flex flex-col justify-center items-center">
             <h1>Calender</h1>
             <p>Coming Soon!</p>
+        </div>
         </>
     )
 }
--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,10 +1,12 @@
 const Tasks = () => {
-    return(
-        <>
-            <h1>Tasks</h1>
-            <p>Coming Soon!</p>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="w-full flex flex-col justify-center items-center">
+        <h1>Tasks</h1>
+        <p>Coming Soon!</p>
+      </div>
+    </>
+  );
+};
 
 export default Tasks;
