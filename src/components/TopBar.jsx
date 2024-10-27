e877551ccbde2f6ce25b1a81afed0454751e2170
Tevin Campbell <tevincampbell73@yahoo.com>
2024-10-27 07:07:05 -0400

    passing props for primary button

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,13 +1,18 @@
 import PrimaryBtn from "./PrimaryBtn";
+import PropTypes from "prop-types";
 
-const TopBar = () => {
+const TopBar = ({ handleModalView }) => {
     return(
         <>
         <div className="w-full p-3 items-center flex">
-            <PrimaryBtn />
+            <PrimaryBtn handleModalView={handleModalView}/>
         </div>
         </>
     )
+}
+
+TopBar.propTypes = {
+     handleModalView: PropTypes.func.isRequired
 }
 
 export default TopBar;
